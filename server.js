286ffbbcf0e060c31c280da1d3f75a1604ea0026
Tracy Kaur <tracykaur@yahoo.com>
2019-02-24 23:03:18 +1000

    practice on server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var server = express();
+
 var staticAssets = express.static('public');
 server.use(staticAssets);
-server.get('/hello', function(request, response) {
-    response.send('hello there');
+server.get('/', function(request, response) {
+    response.render('pages/index');
   });
+  
 server.listen(3000, function () {
   console.log('Server has started listening on port 3000.');
 });
