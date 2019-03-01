2b095c3d24adcad8edaa1d2520bebceafa8985ec
Tracy Kaur <tracykaur@yahoo.com>
2019-03-01 23:11:19 +1000

    added pages

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ server.use(staticAssets);
 server.get('/', function(request, response) {
     response.render('pages/index');
   });
-  
+  server.get('/recipe/:id', function(request, response) {
+    response.send('recipe id: '+ request.params.id); 
+  });
 server.listen(3000, function () {
   console.log('Server has started listening on port 3000.');
 });
